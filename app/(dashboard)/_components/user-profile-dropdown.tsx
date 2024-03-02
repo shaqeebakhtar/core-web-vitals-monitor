3033165c6a6dbe700f335000caafb5f90fcddc78
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-03-02 22:31:55 +0530

    remove fixed email from profile dropdown

--- app/(dashboard)/_components/user-profile-dropdown.tsx
+++ app/(dashboard)/_components/user-profile-dropdown.tsx
@@ -15,11 +15,20 @@ import { generateAvatar } from '@/lib/random-avatar';
 import { LogOut, MessageCircleQuestion, Settings } from 'lucide-react';
 import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import { ReactNode, useEffect, useState } from 'react';
 
 const UserProfileDropdown = () => {
   const { data: session } = useSession();
+  const { slug } = useParams() as { slug?: string };
+  const [avatar, setAvatar] = useState<ReactNode>('');
 
-  const avatar = generateAvatar(session?.user.name || 'random');
+  useEffect(() => {
+    if (session?.user?.name) {
+      const generatedAvatar = generateAvatar(session.user.name);
+      setAvatar(generatedAvatar);
+    }
+  }, [session]);
 
   return (
     <DropdownMenu>
@@ -35,20 +44,18 @@ const UserProfileDropdown = () => {
         <DropdownMenuLabel>
           <div className="flex flex-col space-y-1 overflow-hidden">
             <p className="text-xs text-gray-700 font-normal">Logged in as</p>
-            <p className="font-medium truncate">
-              {session?.user?.email || 'shaqeeb@gmail.com'}
-            </p>
+            <p className="font-medium truncate">{session?.user?.email}</p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <Link href={`/${session?.user.name}`}>
+          <Link href={`/${slug}/support`}>
             <DropdownMenuItem>
               <MessageCircleQuestion className="w-4 h-4 mr-2 text-gray-700" />
               <span className="font-normal">Support</span>
             </DropdownMenuItem>
           </Link>
-          <Link href="/settings">
+          <Link href={`/${slug}/settings`}>
             <DropdownMenuItem>
               <Settings className="w-4 h-4 mr-2 text-gray-700" />
               <span className="font-normal">Settings</span>
