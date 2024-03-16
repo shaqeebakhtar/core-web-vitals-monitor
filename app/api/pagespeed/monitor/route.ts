dd8a2207ab7c2e9723faf45e8c750b0a9622b18a
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-03-16 14:59:53 +0530

    save monitoring data

--- app/api/pagespeed/monitor/route.ts
+++ app/api/pagespeed/monitor/route.ts
@@ -1,6 +1,15 @@
 import { authOptions } from '@/lib/auth/options';
 import { monitorSchema } from '@/schemas/monitor';
-import { createMonitor } from '@/services/monitor';
+import {
+  createMonitor,
+  getLatestMetrics,
+  getLatestScores,
+  getMetricsHistory,
+  getMonitorById,
+  getScoresHistory,
+  updateMonitor,
+} from '@/services/monitor';
+import { getPagespeedResult } from '@/services/pagespeed';
 import { getServerSession } from 'next-auth';
 
 export const POST = async (req: Request) => {
@@ -33,4 +42,55 @@ export const POST = async (req: Request) => {
   });
 
   return Response.json({ monitorId: monitor.id }, { status: 200 });
+};
+
+export const PATCH = async (req: Request) => {
+  const body = await req.json();
+  const { monitorId, url, device } = body;
+
+  const monitor = await getMonitorById(monitorId);
+
+  if (!monitor) {
+    return Response.json(
+      {
+        message: 'No monitoring found',
+      },
+      { status: 404 }
+    );
+  }
+
+  const pagespeedResult = await getPagespeedResult({ url, device });
+
+  const latestMetrics = getLatestMetrics(pagespeedResult);
+
+  const latestScores = getLatestScores(pagespeedResult);
+
+  const scoresHistory = await getScoresHistory(
+    pagespeedResult,
+    monitor.scoresHistory
+  );
+
+  const metricsHistory = await getMetricsHistory(
+    pagespeedResult,
+    monitor.metricsHistory
+  );
+
+  updateMonitor({
+    monitorId,
+    latestMetrics: JSON.stringify(latestMetrics),
+    latestScores: JSON.stringify(latestScores),
+    scoresHistory: JSON.stringify(scoresHistory),
+    metricsHistory: JSON.stringify(metricsHistory),
+    lastFetchedAt: pagespeedResult.lighthouseResult.fetchTime,
+  });
+
+  return Response.json(
+    {
+      latestMetrics,
+      latestScores,
+      scoresHistory,
+      metricsHistory,
+    },
+    { status: 200 }
+  );
 };
--- services/monitor.ts
+++ services/monitor.ts
@@ -11,6 +11,9 @@ export const getAllMonitorsByProjectId = async (projectId: string) => {
     where: {
       projectId,
     },
+    orderBy: {
+      createdAt: 'desc',
+    },
   });
 };
 
@@ -35,4 +38,214 @@ export const createMonitor = async ({
   } catch (error) {
     throw new Error('Failed to create monitor');
   }
+};
+
+type updateMonitorSchema = {
+  monitorId: string;
+  latestMetrics: string;
+  latestScores: string;
+  scoresHistory: string;
+  metricsHistory: string;
+  lastFetchedAt: Date;
+};
+
+export const updateMonitor = async ({
+  monitorId,
+  latestMetrics,
+  latestScores,
+  scoresHistory,
+  metricsHistory,
+  lastFetchedAt,
+}: updateMonitorSchema) => {
+  return await db.monitor.update({
+    where: { id: monitorId },
+    data: {
+      latestMetrics,
+      latestScores,
+      scoresHistory,
+      metricsHistory,
+      lastFetchedAt,
+    },
+  });
+};
+
+export const getMonitorById = async (monitorId: string) => {
+  return await db.monitor.findUnique({
+    where: {
+      id: monitorId,
+    },
+  });
+};
+
+export const getLatestMetrics = (result: any) => {
+  const metrics = {} as any;
+
+  const metricKeys = [
+    'cumulative-layout-shift',
+    'first-contentful-paint',
+    'largest-contentful-paint',
+    'total-blocking-time',
+    'speed-index',
+  ];
+
+  metricKeys.forEach((key) => {
+    const value = result.lighthouseResult.audits[key as string];
+    metrics[key as string] = {
+      score: value.score,
+      displayValue: value.displayValue,
+      title: value.title,
+    };
+  });
+
+  return metrics;
+};
+
+export const getLatestScores = (result: any) => {
+  const scores = {} as any;
+
+  const scoreKeys = [
+    'performance',
+    'accessibility',
+    'best-practices',
+    'seo',
+    'pwa',
+  ];
+
+  scoreKeys.forEach((key) => {
+    const value = result.lighthouseResult.categories[key as string];
+    scores[key as string] = {
+      score: value.score,
+      title: value.title,
+    };
+  });
+
+  return scores;
+};
+
+export const getScoresHistory = async (
+  result: any,
+  prevScoresHistory: string | null
+) => {
+  // performance score
+  const latestPerformance = {
+    score: result.lighthouseResult.categories['performance'].score,
+    fetchedAt: result.lighthouseResult.fetchTime,
+  };
+
+  // accessibility score
+  const latestAccessibility = {
+    score: result.lighthouseResult.categories['accessibility'].score,
+    fetchedAt: result.lighthouseResult.fetchTime,
+  };
+
+  // best practices score
+  const latestBestPractices = {
+    score: result.lighthouseResult.categories['best-practices'].score,
+    fetchedAt: result.lighthouseResult.fetchTime,
+  };
+
+  // seo score
+  const latestSeo = {
+    score: result.lighthouseResult.categories['seo'].score,
+    fetchedAt: result.lighthouseResult.fetchTime,
+  };
+
+  // pwa score
+  const latestPwa = {
+    score: result.lighthouseResult.categories['pwa'].score,
+    fetchedAt: result.lighthouseResult.fetchTime,
+  };
+
+  let scoresHistory;
+
+  if (prevScoresHistory === null) {
+    scoresHistory = {} as any;
+
+    scoresHistory['performance'] = [latestPerformance];
+    scoresHistory['accessibility'] = [latestAccessibility];
+    scoresHistory['bestPractices'] = [latestBestPractices];
+    scoresHistory['seo'] = [latestSeo];
+    scoresHistory['pwa'] = [latestPwa];
+  } else {
+    scoresHistory = await JSON.parse(prevScoresHistory);
+
+    scoresHistory.performance.push(latestPerformance);
+    scoresHistory.accessibility.push(latestAccessibility);
+    scoresHistory.bestPractices.push(latestBestPractices);
+    scoresHistory.seo.push(latestSeo);
+    scoresHistory.pwa.push(latestPwa);
+  }
+
+  return scoresHistory;
+};
+
+export const getMetricsHistory = async (
+  result: any,
+  prevMetricsHistory: string | null
+) => {
+  // fcp score
+  const latestFCP = {
+    score: result.lighthouseResult.audits['first-contentful-paint'].score,
+    displayValue:
+      result.lighthouseResult.audits['first-contentful-paint'].displayValue,
+    fetchedAt: result.lighthouseResult.fetchTime,
+    title: result.lighthouseResult.audits['first-contentful-paint'].title,
+  };
+
+  // lcp score
+  const latestLCP = {
+    score: result.lighthouseResult.audits['largest-contentful-paint'].score,
+    displayValue:
+      result.lighthouseResult.audits['largest-contentful-paint'].displayValue,
+    fetchedAt: result.lighthouseResult.fetchTime,
+    title: result.lighthouseResult.audits['largest-contentful-paint'].title,
+  };
+
+  // tbt score
+  const latestTBT = {
+    score: result.lighthouseResult.audits['total-blocking-time'].score,
+    displayValue:
+      result.lighthouseResult.audits['total-blocking-time'].displayValue,
+    fetchedAt: result.lighthouseResult.fetchTime,
+    title: result.lighthouseResult.audits['total-blocking-time'].title,
+  };
+
+  // cls score
+  const latestCLS = {
+    score: result.lighthouseResult.audits['cumulative-layout-shift'].score,
+    displayValue:
+      result.lighthouseResult.audits['cumulative-layout-shift'].displayValue,
+    fetchedAt: result.lighthouseResult.fetchTime,
+    title: result.lighthouseResult.audits['cumulative-layout-shift'].title,
+  };
+
+  // speed index score
+  const latestSpeedIndex = {
+    score: result.lighthouseResult.audits['speed-index'].score,
+    displayValue: result.lighthouseResult.audits['speed-index'].displayValue,
+    fetchedAt: result.lighthouseResult.fetchTime,
+    title: result.lighthouseResult.audits['speed-index'].title,
+  };
+
+  let metricsHistory;
+
+  if (prevMetricsHistory === null) {
+    metricsHistory = {} as any;
+
+    metricsHistory['fcp'] = [latestFCP];
+    metricsHistory['lcp'] = [latestLCP];
+    metricsHistory['cls'] = [latestTBT];
+    metricsHistory['cls'] = [latestCLS];
+    metricsHistory['speedIndex'] = [latestSpeedIndex];
+  } else {
+    metricsHistory = await JSON.parse(prevMetricsHistory);
+
+    metricsHistory.fcp.push(latestFCP);
+    metricsHistory.lcp.push(latestLCP);
+    metricsHistory.cls.push(latestTBT);
+    metricsHistory.cls.push(latestCLS);
+    metricsHistory.speedIndex.push(latestSpeedIndex);
+  }
+
+  return metricsHistory;
 };
